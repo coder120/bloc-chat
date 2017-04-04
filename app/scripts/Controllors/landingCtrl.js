286d6e29a8a88770a242e1a01fd444d6073adf19
khan124 <gooduraiz_khan@yahoo.com>
2017-04-04 15:10:35 -0700

    checkpoint 2

---
+++ app/scripts/Controllors/landingCtrl.js
@@ -0,0 +1,10 @@
+(function() {
+    function landingCtrl(Room) {
+        this.rooms = Room.all;
+        
+    }
+    
+    angular
+        .module('blocChat')
+        .controller('landingCtrl', ["Room", landingCtrl]);
+})();
